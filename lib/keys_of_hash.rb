119445e4943ad2ec7fd3797a885e334f30fbe25b
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-04-03 17:14:35 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,9 @@ class Hash
   def keys_of(*arguments)
     countries = self.values 
     animals = self.keys 
+    animal = nil 
     array = []
+    self[arguments]
     binding.pry 
     
       return array
