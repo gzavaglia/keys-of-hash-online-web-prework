1d49c91c61c8314cd753f7548938a9a17939e011
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-04-03 18:15:36 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,30 @@
+require 'pry'
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
-end
+  def keys_of(*arguments)
+    countries = self.values 
+    animals = self.keys 
+    array = []
+    count = 0 
+    unless count > self.size + 1  
+    country = arguments[count]
+    country2 = arguments[count+1]
+    self.each do |pet, place|
+      if country == place
+        animal = pet
+        array.push(animal)
+        count = count + 1 
+      elsif country2 == place
+        animal2 = pet
+        array.push(animal2)
+        count = count + 1
+      else 
+        count = count + 1
+       
+      end #end if 
+      
+    end #end do 
+    end #while  
+    return array
+     
+  end #end method 
+end #end class 
